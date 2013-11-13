dac8427b925cbd9163c800b954fe786089b0ec32
Jongeun Lee <yomybaby@yomybaby.com>
2013-11-13 12:34:12 +0900

    imageview, listview

---
+++ TiUIImageView+IsCustom.h
@@ -0,0 +1,14 @@
+//
+//  TiUIImageView+IsCustom.h
+//  linqlemodule
+//
+//  Created by yomybaby on 12. 12. 13..
+//
+//
+
+#import "TiUIView.h"
+#import "TiUIImageView.h"
+
+@interface TiUIImageView (IsCustom)
+
+@end
